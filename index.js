033451fd8e1cc903ae445442174e746a768338ba
Flo Leeroy <florian.leeroy@hotmail.com>
2021-02-21 02:13:25 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,79 @@
+/**
+ * All the libraries
+ */
+const moment = require('moment')
+const express = require('express')
+const Handlebars = require('handlebars')
+const handlebars = require('express-handlebars')
+
+/**
+ * My own libraries
+ */
+const Membres = require('./modules/membres')
+
+
+/**
+ * Setup the handlebars lib
+ */
+const app = express()
+app.use(express.urlencoded({extended: true}))
+app.set("view engine", "handlebars")
+app.engine("handlebars", handlebars({
+    layoutsDir: __dirname + "/views/layouts/",
+    partialsDir: __dirname + "/views/partials/",
+}))
+
+/**
+ * Pages
+ */
+function getPage(page, req, res){
+    if(page != "error"){
+        console.log(`${req.headers["x-forwarded-for"] || req.connection.remoteAddress} asked for ${page}`)
+    }
+
+    res.render(page, {layout: "layout"})
+}
+
+app.get("/", function(req, res){
+    getPage("index", req, res)
+})
+app.get("/aboutus", function(req, res){
+    getPage("aboutus", req, res)
+})
+app.get("/membres", function(req, res){
+    Membres.getPage(req, res)
+})
+app.get("/projets", function(req, res){
+    getPage("projets", req, res)
+})
+app.get("/recrutement", function(req, res){
+    getPage("recrutement", req, res)
+})
+app.get("/stream", function(req, res){
+    getPage("stream", req, res)
+})
+app.get("/error", function(req, res){
+    getPage("error", req, res)
+})
+
+/**
+ * Start server
+ */
+app.use(express.static("public"))
+app.use(function (req, res){
+	res.status(404)
+    res.redirect("error")
+})
+app.listen(1000, function(){
+    console.log("Server running on port 1000!")
+})
+
+/**
+ * Add date to console.log
+ */
+const basicConsole = console.log
+console.log = function(){
+    const date = `[${moment(new Date()).format("DD/MM/yyyy - HH:mm:ss")}]`
+    Array.prototype.unshift.call(arguments, date)
+    basicConsole.apply(this, arguments)
+}
---
+++ modules/membres.js
@@ -0,0 +1,50 @@
+const fs = require('fs')
+class Membre{
+    constructor(pseudo, roles){
+        this.pseudo = pseudo
+        this.roles = roles
+    }
+}
+
+const admins = new Array(
+    new Membre("Zyguan", new Array("Président", "Joueur")),
+    new Membre("Shuku", new Array("Co-Président")),
+    new Membre("Konogan", new Array("Manager TFT", "Streamer", "Joueur")),
+    new Membre("Wazzy", new Array("Manager Stream", "Streamer")),
+    new Membre("Floliroy", new Array("Graphiste", "Web Dev", "Streamer", "Joueur")),
+    new Membre("Cripito", new Array("Orga Event")),
+    new Membre("Nairod", new Array("Orga Event", "Joueur")),
+    new Membre("Gazalhi", new Array("Community Manager")),
+)
+const joueurs = new Array(
+    new Membre("Aaron", new Array("Joueur")),
+    new Membre("Crazy Genius", new Array("Joueur")),
+    new Membre("Strelok", new Array("Joueur")),
+    new Membre("ACKK", new Array("Joueur")),
+    new Membre("Frobei", new Array("Joueur")),
+    new Membre("Martin", new Array("Joueur")),
+    new Membre("Rakluuhr", new Array("Streamer", "Joueur")),
+    new Membre("Ronflex", new Array("Streamer", "Joueur")),
+    new Membre("Sirutop", new Array("Joueur")),
+    new Membre("Tragoedia", new Array("Joueur")),
+    new Membre("Verigular", new Array("Joueur")),
+    new Membre("Zel", new Array("Streamer", "Joueur")),
+)
+
+module.exports = class Membres{
+
+    static getPage(req, res){
+        console.log(`${req.headers["x-forwarded-for"] || req.connection.remoteAddress} asked for amongus`)
+        res.render("membres", {layout: "layout",
+            admins: admins,
+            joueurs: joueurs,
+            helpers: {
+                getImage: function(name){
+                    const path = `${__dirname.replace("modules", "")}public/images/${name.toLowerCase().replace(" ", "")}.png`
+                    return fs.existsSync(path) ? `./images/${name.toLowerCase().replace(" ", "")}.png` : "./logo.png"
+                }
+            }
+        })
+    }
+
+}
