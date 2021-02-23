4c27f70ebd6d7e123e87ab6cc7360f942cc22aa0
Flo Leeroy <florian.leeroy@hotmail.com>
2021-02-23 02:11:58 +0100

    add some twitter

--- modules/membres.js
+++ modules/membres.js
@@ -12,28 +12,28 @@ class Membre{
 
 const admins = new Array(
     new Membre("Zyguan", new Array("Président", "Joueur"), null,
-        null, null, null, null
+        "https://twitter.com/TZyguan", null, null, null
     ),
     new Membre("Shuku", new Array("Co-Président"), null,
-        null, null, null, null
+        "https://twitter.com/TMSShuku", null, null, null
     ),
     new Membre("Konogan", new Array("Manager TFT", "Streamer", "Joueur"), null,
-        null, null, null, null
+        "https://twitter.com/Konogan_", null, null, null
     ),
     new Membre("Wazzy", new Array("Manager Stream", "Streamer"), null,
-        null, null, null, null
+        "https://twitter.com/TmsWazzy", null, null, null
     ),
     new Membre("Floliroy", new Array("Graphiste", "Web Dev", "Streamer", "Joueur"), `./images/floliroy.png`,
         "https://twitter.com/Floliroy", "https://www.facebook.com/floliroyjenkins", null, "https://www.twitch.tv/floleeroyj"
     ),
     new Membre("Cripito", new Array("Orga Event"), null,
-        null, null, null, null
+        "https://twitter.com/cripito_", null, null, null
     ),
     new Membre("Nairod", new Array("Orga Event", "Joueur"), null,
-        null, null, null, null
+        "https://twitter.com/NairodTV", null, null, null
     ),
     new Membre("Gazalhi", new Array("Community Manager"), null,
-        null, null, null, null
+        "https://twitter.com/Hg1er", null, null, null
     ),
 )
 const joueurs = new Array(
@@ -41,37 +41,40 @@ const joueurs = new Array(
         null, null, null, null
     ),
     new Membre("Crazy Genius", new Array("Joueur"), null,
-        null, null, null, null
+        "https://twitter.com/CrazyGeniusTV", null, null, "https://www.twitch.tv/crazygeniustv"
     ),
     new Membre("Strelok", new Array("Joueur"), null,
-        null, null, null, null
+        "https://twitter.com/StrelokTms", null, null, null
     ),
     new Membre("ACKK", new Array("Joueur"), null,
-        null, null, null, null
+        "https://twitter.com/tftackk", null, null, null
     ),
     new Membre("Frobei", new Array("Joueur"), null,
-        null, null, null, null
+        "https://twitter.com/FrobeiF", null, null, null
     ),
     new Membre("Martin", new Array("Joueur"), null,
         null, null, null, null
     ),
     new Membre("Rakluuhr", new Array("Streamer", "Joueur"), null,
-        null, null, null, null
+        "https://twitter.com/Rakluuhr", null, null, null
     ),
     new Membre("Ronflex", new Array("Streamer", "Joueur"), null,
-        null, null, null, null
+        "https://twitter.com/JOOOOOOORDY_", null, null, null
     ),
     new Membre("Sirutop", new Array("Joueur"), null,
         null, null, null, null
+    ),
+    new Membre("Shacovid", new Array("Joueur"), null,
+        "https://twitter.com/shacovid", null, null, null
     ),
     new Membre("Tragoedia", new Array("Joueur"), null,
         null, null, null, null
     ),
     new Membre("Verigular", new Array("Joueur"), null,
-        null, null, null, null
+        "https://twitter.com/verigular", null, null, null
     ),
     new Membre("Zel", new Array("Streamer", "Joueur"), null,
-        null, null, null, null
+        "https://twitter.com/TMSZel", null, null, null
     ),
 )
 
