d05977fd7dfe60b5209926918544b5c6c1ebaa27
Flo Leeroy <florian.leeroy@hotmail.com>
2021-02-25 14:25:00 +0100

    use barbaJs

--- public/dynamic.js
+++ public/dynamic.js
@@ -16,6 +16,59 @@ window.setTimeout(offsetAnchor, 0)
 /**
  * Set page transition
  */
-window.addEventListener("beforeunload", function () {
-    document.getElementById("main").classList.add("animateOut");
+let loader = $(".loader")
+function loaderIn() {
+    return gsap.fromTo(loader,
+        {
+            rotation: 30,
+            scaleX: 0,
+            xPercent: -5
+        },
+        {
+            duration: 0.8,
+            xPercent: 0,
+            scaleX: 1,
+            rotation: 0,
+            ease: 'power4.inOut',
+            transformOrigin: 'left center'
+        })
+}
+function loaderAway() {
+    return gsap.to(loader, {
+        duration: 0.8,
+        scaleX: 0,
+        xPercent: 5,
+        rotation: 0,
+        transformOrigin: 'right center',
+        ease: 'power4.inOut'
+    })
+}
+gsap.set(loader, {
+    scaleX: 0,
+    rotation: 30,
+    xPercent: -5,
+    yPercent: -50,
+    transformOrigin: 'left center',
+    autoAlpha: 1
+})
+barba.hooks.enter(() => {
+    window.scrollTo(window.scrollX, 0);
+})
+barba.init({
+    transitions: [{
+        async leave() {
+            await loaderIn();
+        },
+        enter() {
+            loaderAway();
+        }
+    }]
+})
+
+/**
+ * Change header active link
+ */
+$(document).on('click', 'header a', function(event) {
+    $(".selected").removeClass("selected")
+    $(event.currentTarget).addClass("selected")
 })
