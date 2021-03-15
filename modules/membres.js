3f9fc8c4e894bc9e6c3275e3a5251251fa932371
Flo Leeroy <florian.leeroy@hotmail.com>
2021-03-15 14:47:34 +0100

    use google sheet to store membres

--- modules/membres.js
+++ modules/membres.js
@@ -1,87 +1,53 @@
+const {GoogleSpreadsheet} = require('google-spreadsheet')
+const doc = new GoogleSpreadsheet("15soO9myOe0mOV9uhvr-lM2PsEow74KRRDS4l4FhL7PQ")
+doc.useServiceAccountAuth({
+    client_email: process.env.GOOGLE_EMAIL,
+    private_key: process.env.GOOGLE_TOKEN.replace(/\\n/g, '\n')
+})
+doc.loadInfo()
+
 class Membre{
-    constructor(pseudo, roles, image, twitter, facebook, instagram, twitch){
+    constructor(pseudo){
         this.pseudo = pseudo
-        this.roles = roles
-        this.image = image
-        this.twitter = twitter
-        this.facebook = facebook
-        this.instagram = instagram
-        this.twitch = twitch
+        this.roles = new Array()
+        this.image = null
+        this.twitter = null
+        this.facebook = null
+        this.instagram = null
+        this.twitch = null
     }
 }
 
-const admins = new Array(
-    new Membre("Zyguan", new Array("Président", "Joueur"), null,
-        "https://twitter.com/TZyguan", null, null, null
-    ),
-    new Membre("Shuku", new Array("Co-Président"), "https://cdn.discordapp.com/attachments/788647635779059733/815683371392565278/51V8QGKL_400x400.png",
-        "https://twitter.com/TMSShuku", null, null, null
-    ),
-    new Membre("Konogan", new Array("Manager TFT", "Streamer", "Joueur"), null,
-        "https://twitter.com/Konogan_", null, null, null
-    ),
-    new Membre("Wazzy", new Array("Manager Stream", "Streamer"), null,
-        "https://twitter.com/TmsWazzy", null, null, null
-    ),
-    new Membre("Floliroy", new Array("Graphiste", "Web Dev", "Streamer", "Joueur"), `./images/floliroy.png`,
-        "https://twitter.com/Floliroy", "https://www.facebook.com/floliroyjenkins", null, "https://www.twitch.tv/floleeroyj"
-    ),
-    new Membre("Cripito", new Array("Orga Event"), null,
-        "https://twitter.com/cripito_", null, null, null
-    ),
-    new Membre("Nairod", new Array("Orga Event", "Joueur"), null,
-        "https://twitter.com/NairodTV", null, null, null
-    ),
-    new Membre("Gazalhi", new Array("Community Manager"), "https://pbs.twimg.com/profile_images/1277300545577979905/oHaP1uae.jpg",
-        "https://twitter.com/Hg1er", null, null, null
-    ),
-)
-const joueurs = new Array(
-    new Membre("Aaron", new Array("Joueur"), null,
-        null, null, null, null
-    ),
-    new Membre("Crazy Genius", new Array("Joueur"), "https://pbs.twimg.com/profile_images/1354444895872831489/_Ttc0_Mz.jpg",
-        "https://twitter.com/CrazyGeniusTV", null, null, "https://www.twitch.tv/crazygeniustv"
-    ),
-    new Membre("Strelok", new Array("Joueur"), "https://pbs.twimg.com/profile_images/1351477080765190148/jhXspoiq.jpg",
-        "https://twitter.com/StrelokTms", null, null, null
-    ),
-    new Membre("ACKK", new Array("Joueur"), null,
-        "https://twitter.com/tftackk", null, null, null
-    ),
-    new Membre("Frobei", new Array("Joueur"), "https://pbs.twimg.com/profile_images/1254013846337007616/Z9KM3ITo.jpg",
-        "https://twitter.com/FrobeiF", null, null, "https://www.twitch.tv/frobei"
-    ),
-    new Membre("Martin", new Array("Joueur"), null,
-        null, null, null, null
-    ),
-    new Membre("Rakluuhr", new Array("Streamer", "Joueur"), null,
-        "https://twitter.com/Rakluuhr", null, null, null
-    ),
-    new Membre("Ronflex", new Array("Streamer", "Joueur"), null,
-        "https://twitter.com/JOOOOOOORDY_", null, null, null
-    ),
-    new Membre("Sirutop", new Array("Joueur"), null,
-        null, null, null, null
-    ),
-    new Membre("Shacovid", new Array("Joueur"), null,
-        "https://twitter.com/shacovid", null, null, null
-    ),
-    new Membre("Tragoedia", new Array("Joueur"), null,
-        null, null, null, null
-    ),
-    new Membre("Verigular", new Array("Streamer", "Joueur"), "https://pbs.twimg.com/profile_images/1287935665557196800/m1JwAGu2.jpg",
-        "https://twitter.com/verigular", null, null, null
-    ),
-    new Membre("Zel", new Array("Joueur"), "https://pbs.twimg.com/profile_images/1311224786543050753/ZyiMstNK.jpg",
-        "https://twitter.com/TMSZel", null, null, null
-    ),
-)
+const admins = new Array()
+const joueurs = new Array()
 
 module.exports = class Membres{
 
-    static getPage(req, res){
+    static async getPage(req, res){
         console.log(`${req.headers["x-forwarded-for"] || req.connection.remoteAddress} asked for membres`)
+
+        const sheet = doc.sheetsById["0"]
+        const rows = await sheet.getRows()
+        for await(let row of rows){
+            let membre = new Membre(row.Pseudo)
+            for(let i=1 ; i<=4 ; i++){
+                if(row[`Role ${i}`]){
+                    membre.roles.push(row[`Role ${i}`])
+                }
+            }
+            if(row.Image) membre.image = row.Image
+            if(row.Twitter) membre.twitter = row.Twitter
+            if(row.Facebook) membre.facebook = row.Facebook
+            if(row.Twitch) membre.twitch = row.Twitch
+            if(row.Instagram) membre.instagram = row.Instagram
+            
+            if(row.Staff == "TRUE"){
+                admins.push(membre)
+            }else{
+                joueurs.push(membre)
+            }
+        }
+
         res.render("partials/layout", {body: "membres",
             admins: admins,
             joueurs: joueurs,
