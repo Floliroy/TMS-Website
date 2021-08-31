974ee27dc1b6ac604925e87362aec872036290ec
Flo Leeroy <florian.leeroy@hotmail.com>
2021-08-31 09:12:17 +0200

    Update barba.js

--- public/scripts/barba.js
+++ public/scripts/barba.js
@@ -45,6 +45,7 @@ barba.hooks.enter(function(){
         window.$("#tableResultats").DataTable({
             "language": {"url": "https://cdn.datatables.net/plug-ins/1.11.0/i18n/fr_fr.json"},
             "retrieve": true,
+            "pageLength": 25,
             "columnDefs": [
                 {type: "date-eu", targets: 0}
             ],
