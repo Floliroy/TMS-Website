83798ebe5970178f5e1dded018477e65723698d8
Flo Leeroy <florian.leeroy@hotmail.com>
2021-08-31 09:10:55 +0200

    better css

--- public/scripts/dynamic.js
+++ public/scripts/dynamic.js
@@ -60,6 +60,7 @@ $(document).ready(function(){
         window.$("#tableResultats").DataTable({
             "language": {"url": "https://cdn.datatables.net/plug-ins/1.11.0/i18n/fr_fr.json"},
             "retrieve": true,
+            "pageLength": 25,
             "columnDefs": [
                 {type: "date-eu", targets: 0}
             ],
